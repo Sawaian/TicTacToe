72b834aa35d4671848849bc5438a6a57a6771bd4
Keith <Keith.macario@gmail.com>
2021-08-04 23:29:21 -0700

    Overhauled the code. It's bigger, uglieir, but it works. Each click adds a property to the object in the gameBoard Array. The win condition works on a == check.

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const gameBoard = (() => {
         [` `], [` `], [` `], 
     ]
 
+    const x =["x"]
+    const o = ["o"];
+
     const winConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -19,34 +22,90 @@ const gameBoard = (() => {
         [2, 4, 6],
     ];
 
-    let checkWin = function(){
-        if(winConditions[0,1,2] === gameBoardArray){
+    let checkWin = function(conditions){
+        if(gameBoardArray[0, 1, 2] == x){ 
             console.log("Win");
         };
     };
 
-    square.forEach(square => addEventListener('click', () => {
-        console.log("listening");
-    }));
+    let spaceOne = document. getElementById("spaceOne");
+    let spacetwo = document.getElementById("spaceTwo");
+    let spaceThree = document. getElementById("spaceThree");
+    let spaceFour = document.getElementById("spaceFour");
+    let spaceFive = document. getElementById("spaceFive");
+    let spaceSix = document.getElementById("spaceSix");
+    let spaceSeven = document. getElementById("spaceSeven");
+    let spaceEight = document.getElementById("spaceEight");
+    let spaceNine = document.getElementById("spaceNine");
+
+    spaceOne.addEventListener('click', ()=> {
+        if(spaceOne.textContent === ""){
+        spaceOne.textContent = gameFlow.turn();
+        gameBoardArray[0] = spaceOne.textContent;
+        console.log(gameBoardArray[0]);}
+        checkWin();
+    });
+
+    spacetwo.addEventListener('click', ()=> {
+        if(spaceTwo.textContent === ""){
+        spaceTwo.textContent = gameFlow.turn();
+        gameBoardArray[1] = spaceTwo.textContent;
+        console.log(gameBoardArray[1]);}
+    });
+
+    spaceThree.addEventListener('click', ()=> {
+        if(spaceThree.textContent === ""){
+        spaceThree.textContent = gameFlow.turn();
+        gameBoardArray[2] = spaceThree.textContent;
+        console.log(gameBoardArray[2]);}
+    });
+
+    spaceFour.addEventListener('click', ()=> {
+        if(spaceFour.textContent === ""){
+        spaceFour.textContent = gameFlow.turn();
+        gameBoardArray[3] = spaceFour.textContent;
+        console.log(gameBoardArray[3]);}
+    });
+
+    spaceFive.addEventListener('click', ()=> {
+        if(spaceFive.textContent === ""){
+        spaceFive.textContent = gameFlow.turn();
+        gameBoardArray[4] = spaceFive.textContent;
+        console.log(gameBoardArray[4]);}
+    });
+
+    spaceSix.addEventListener('click', ()=> {
+        if(spaceSix.textContent === ""){
+        spaceSix.textContent = gameFlow.turn();
+        gameBoardArray[5] = spaceSix.textContent;
+        console.log(gameBoardArray[5]);}
+    });
+
+    spaceSeven.addEventListener('click', ()=> {
+        if(spaceSeven.textContent === ""){
+        spaceSeven.textContent = gameFlow.turn();
+        gameBoardArray[6] = spaceSeven.textContent;
+        console.log(gameBoardArray[6]);}
+    });
+
+    spaceEight.addEventListener('click', ()=> {
+        if(spaceEight.textContent === ""){
+        spaceEight.textContent = gameFlow.turn();
+        gameBoardArray[7] = spaceEight.textContent;
+        console.log(gameBoardArray[7]);}
+    });
+
+    spaceNine.addEventListener('click', ()=> {
+        if(spaceNine.textContent === ""){
+        spaceNine.textContent = gameFlow.turn();
+        gameBoardArray[8] = spaceNine.textContent;
+        console.log(gameBoardArray[8]);}
+    });
 
     gameBoardArray.forEach(element => {
 
-            // square.addEventListener('click',() => {
-            //     console.log("listening");
-            //     if(square.textContent === ""){
-            //         element = gameFlow.turn();
-            //     square.textContent = element;
-
-            //     console.table(gameBoardArray);
-            //     console.log(element);
-
-            
-            // }
-            // });
-            
-    });
-
     
+    });
     
 
 
@@ -70,7 +129,6 @@ let playerTwo = player('jacob', "o");
 
 const gameFlow = (() =>{
 
-    let gameSquare = document.querySelectorAll("#gameBoardSquare")
     let playerTurn = false;;
 
      function turn(){ 
@@ -93,10 +151,6 @@ const gameFlow = (() =>{
         };
     };
 
-
-
-    for(let i = 0; i < 9; i++){
-    console.log(currentTurn());}
     return {
         turn
     };
